37352c1d820a13ada7d94d19d8ad9298249742d4
Xadnem53 <xadnem53@gmail.com>
2017-07-15 22:33:28 +0000

    Update Compiler.cmd

--- Compiler.cmd
+++ Compiler.cmd
@@ -8,11 +8,11 @@ SET ruta64=C:\Windows\Microsoft.NET\Framewrk64
 CD %ruta64% 
 ::Save the list of subdirectories sorted alphabetically into a temp.txt file in the user directory.
 DIR /o:n > %USERPROFILE%\temp.txt 
-::Find the previous text file lines that contains the word "DIR" and save them into a temp2.txt file
+::Find the previous text file lines containing the word "DIR" and save them into a temp2.txt file
 TYPE %USERPROFILE%\temp.txt | FIND "DIR" > %USERPROFILE%\temp2.txt
-::Overwrite the previous temp.txt file with the lines in temp2.txt which contains the 'v' character 
+::Overwrite the previous temp.txt file with the lines in temp2.txt that contain the 'v'
 TYPE %USERPROFILE%\temp2.txt | FIND "v" > %USERPROFILE%\text
-::Set into a variable 'directorio', the fourth word at the file temp.txt last line 
+::Set into a variable 'directorio', the fourth word at the file temp.txt's last line 
 	FOR /F "TOKENS=4 DELIMS= " %%x in (%USERPROFILE%\temp.txt) do (
 	SET directorio=%%x
 	)
